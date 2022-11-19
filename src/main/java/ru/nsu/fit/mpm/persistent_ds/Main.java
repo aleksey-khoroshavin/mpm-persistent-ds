fc520ec73ea40ca48d9a6e9e9ee34585a4a537e6
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-11-20 00:12:39 +0700

    mpm-0001 - Инициализация проекта

---
+++ src/main/java/ru/nsu/fit/mpm/persistent_ds/Main.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.mpm.persistent_ds;
+
+public class Main {
+    public static void main(String[] args) {
+        //code sample
+    }
+}
---
+++ src/test/java/ru/nsu/fit/mpm/persistent_ds/TestExample.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.mpm.persistent_ds;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestExample {
+    @Test
+    void testExample() {
+        int a = 1;
+        assertEquals(1, a);
+    }
+}
