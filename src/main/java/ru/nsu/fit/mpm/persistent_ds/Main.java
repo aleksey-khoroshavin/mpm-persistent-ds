ba60da98063bf50be336c16642d41ee3a90b38e7
Дарья Кондакова <darya.kondakova@gmail.com>
2022-12-04 15:36:53 +0700

    MPMPDS-0005 Реализация массива по базовым требованиям

--- src/main/java/ru/nsu/fit/mpm/persistent_ds/Main.java
+++ src/main/java/ru/nsu/fit/mpm/persistent_ds/Main.java
@@ -1,15 +1,11 @@
 package ru.nsu.fit.mpm.persistent_ds;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         PersistentArray<Integer> pa = new PersistentArray<>();
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        pa.add(list);
-        PersistentArray<Integer> pa2 = pa.append(4);
+        System.out.println(pa.root.getChildren().get(0).getChildren().get(0).data);
+        pa.add(1);
+        pa.add(2);
+        System.out.println(pa.root.getChildren().get(0).getChildren().get(0).data);
     }
 }
