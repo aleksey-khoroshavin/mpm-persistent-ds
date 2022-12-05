343a5c508c8d660d0d878c6f6043cf318bb167f9
Дарья Кондакова <darya.kondakova@gmail.com>
2022-12-05 19:54:43 +0700

    MPMPDS-0008 Реализация персистентного списка по базовым требованиям

--- src/main/java/ru/nsu/fit/mpm/persistent_ds/Main.java
+++ src/main/java/ru/nsu/fit/mpm/persistent_ds/Main.java
@@ -2,19 +2,31 @@ package ru.nsu.fit.mpm.persistent_ds;
 
 public class Main {
     public static void main(String[] args) {
-        PersistentArray<Integer> pa = new PersistentArray<>();
+        System.out.println("PersistentArray:");
+        PersistentArray<Integer> persistentArray = new PersistentArray<>();
 
-        System.out.println(pa.root.getChildren().get(0).getChildren().get(0).data);
+        System.out.println(persistentArray.root.getChildren().get(0).getChildren().get(0).data);
         for (int i = 0; i < 18; i++) {
-            pa.add(i);
+            persistentArray.add(i);
         }
-        System.out.println(pa.root.getChildren().get(0).getChildren().get(0).data);
-        System.out.println(pa.root.getChildren().get(0).getChildren().get(1).data);
-        System.out.println(pa.root.getChildren().get(0).getChildren().get(2).data);
-        System.out.println(pa.root.getChildren().get(0).getChildren().get(3).data);
-        System.out.println(pa.root.getChildren().get(1).getChildren().get(0).data);
+        System.out.println(persistentArray.root.getChildren().get(0).getChildren().get(0).data);
+        System.out.println(persistentArray.root.getChildren().get(0).getChildren().get(1).data);
+        System.out.println(persistentArray.root.getChildren().get(1).getChildren().get(0).data);
 
-        System.out.println(pa.get(0));
-        System.out.println(pa.get(1));
+        System.out.println(persistentArray.get(0));
+        System.out.println(persistentArray.get(1));
+        System.out.println(persistentArray.get(2));
+
+        System.out.println("\nPersistentList:");
+
+        PersistentList<Integer> persistentList = new PersistentList<>();
+
+        for (int i = 0; i < 15; i++) {
+            persistentList.add(i);
+        }
+
+        for (int i = 0; i < persistentList.size(); i++) {
+            System.out.println(persistentList.get(i));
+        }
     }
 }
