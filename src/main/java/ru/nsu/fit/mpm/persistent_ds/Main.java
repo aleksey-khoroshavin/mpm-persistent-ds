2f57e2f82edaeceae30e1156bb2c8f317254936b
Дарья Кондакова <darya.kondakova@gmail.com>
2022-12-05 19:28:37 +0700

    MPMPDS-0007 Доработка массива с помощью имплементации списка

--- src/main/java/ru/nsu/fit/mpm/persistent_ds/Main.java
+++ src/main/java/ru/nsu/fit/mpm/persistent_ds/Main.java
@@ -3,9 +3,18 @@ package ru.nsu.fit.mpm.persistent_ds;
 public class Main {
     public static void main(String[] args) {
         PersistentArray<Integer> pa = new PersistentArray<>();
+
         System.out.println(pa.root.getChildren().get(0).getChildren().get(0).data);
-        pa.add(1);
-        pa.add(2);
+        for (int i = 0; i < 18; i++) {
+            pa.add(i);
+        }
         System.out.println(pa.root.getChildren().get(0).getChildren().get(0).data);
+        System.out.println(pa.root.getChildren().get(0).getChildren().get(1).data);
+        System.out.println(pa.root.getChildren().get(0).getChildren().get(2).data);
+        System.out.println(pa.root.getChildren().get(0).getChildren().get(3).data);
+        System.out.println(pa.root.getChildren().get(1).getChildren().get(0).data);
+
+        System.out.println(pa.get(0));
+        System.out.println(pa.get(1));
     }
 }
