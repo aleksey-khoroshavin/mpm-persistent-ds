c8fc102f365eb1f4c507eb4e011ee3e1494bf1a5
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-12-08 21:40:04 +0700

    MPMPDS-0015 Доделана и проверена функция assoc при обновлении массива

--- src/main/java/ru/nsu/fit/mpm/persistent_ds/Node.java
+++ src/main/java/ru/nsu/fit/mpm/persistent_ds/Node.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Node<E> {
-    public static int bitPerNode = 5;
+    public static int bitPerNode = 1;
     public static int width = (int) Math.pow(2, bitPerNode);
 
     public List<Node<E>> child;
@@ -25,13 +25,16 @@ public class Node<E> {
     }
 
     public boolean isEmpty() {
-        if ((child == null) && (value == null))
+        if ((child == null) && (value == null)) {
             return true;
+        }
         boolean result = true;
-        if ((child != null) && (!child.isEmpty()))
+        if ((child != null) && (!child.isEmpty())) {
             result = false;
-        if ((value != null) && (!value.isEmpty()))
+        }
+        if ((value != null) && (!value.isEmpty())) {
             result = false;
+        }
         return result;
     }
 }
--- src/main/java/ru/nsu/fit/mpm/persistent_ds/PersistentArray.java
+++ src/main/java/ru/nsu/fit/mpm/persistent_ds/PersistentArray.java
@@ -12,19 +12,23 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 public class PersistentArray<E> extends AbstractPersistentCollection<E> {
+
     private Stack<Head<E>> undo = new Stack<>();
     private Stack<Head<E>> redo = new Stack<>();
 
+
     public PersistentArray() {
-        super(6);
+        this(6, false);
+    }
+
+    public PersistentArray(int depth, boolean foo) {
+        super(depth);
         Head<E> head = new Head<>();
         undo.push(head);
     }
 
     public PersistentArray(int maxSize) {
-        super((int) Math.ceil(log(maxSize, (int) Math.pow(2, Node.bitPerNode))));
-        Head<E> head = new Head<>();
-        undo.push(head);
+        this((int) Math.ceil(log(maxSize, (int) Math.pow(2, Node.bitPerNode))), false);
     }
 
     @Override
@@ -72,63 +76,84 @@ public class PersistentArray<E> extends AbstractPersistentCollection<E> {
         return result;
     }
 
+    private void printLeafs(Head<E> head) {
+        for (int i = 0; i < head.size; i++) {
+            System.out.print(i + ":" + String.format("%09d", getLeaf(head.root, i).hashCode()) + "; ");
+        }
+        System.out.println();
+    }
+
     public boolean assoc(int index, E value) {
-        if (getCurrentHead().size == maxSize) {
-            return false;
+        if (index >= getCurrentHead().size) {
+            throw new IndexOutOfBoundsException();
         }
 
         Head<E> oldHead = getCurrentHead();
-        Node<E> copedNode = copyNode(oldHead, index, +1);
+        printLeafs(oldHead);
 
-        copedNode.value.add(index, value);
+        Pair<Node<E>, Integer> copedNodeP = copyNode(oldHead, index);
+        int leafIndex = copedNodeP.getValue();
+        Head<E> newHead = getCurrentHead();
+        printLeafs(newHead);
+        Node<E> copedNode = copedNodeP.getKey();
+
+        copedNode.value.add(leafIndex, value);
+        newHead.size += 1;
+
         if (copedNode.value.size() > Node.width) {
             copedNode.value.remove(copedNode.value.size() - 1);
-            for (int i = index + 1; i < oldHead.size; i++) {
-                conj(get(oldHead, i));
+            for (int i = index; i < oldHead.size; i++) {
+                conj(newHead, get(oldHead, i));
             }
         }
+        printLeafs(newHead);
+
 
         return true;
     }
 
-    private Node<E> copyNode(Head<E> head, int insertIndex, int sizeDelta) {
+    private Pair<Node<E>, Integer> copyNode(Head<E> head, int insertIndex) {
         if (getCurrentHead().size == maxSize) {
             throw new IllegalStateException("array is full");
         }
 
-        Head<E> newHead = new Head<>(head, sizeDelta);
+        Head<E> newHead = new Head<>(head, 0);
         undo.push(newHead);
         redo.clear();
         Node<E> currentNode = newHead.root;
         int level = Node.bitPerNode * (depth - 1);
-
         while (level > 0) {
             int index = (insertIndex >> level) & mask;
             Node<E> tmp, newNode;
-
             tmp = currentNode.child.get(index);
             newNode = new Node<>(tmp);
             currentNode.child.set(index, newNode);
-
             currentNode = newNode;
             level -= Node.bitPerNode;
         }
-
-        return currentNode;
+        return new Pair<>(currentNode, insertIndex & mask);
     }
 
     public boolean conj(E newElement) {
-        if (getCurrentHead().size == maxSize) {
-            return false;
-        }
         Head<E> newHead = new Head<>(getCurrentHead(), +1);
         undo.push(newHead);
         redo.clear();
-        Node<E> currentNode = newHead.root;
+
+        return conj(newHead, newElement);
+    }
+
+    private boolean conj(Head<E> head, E newElement) {
+        if (head.size == maxSize) {
+            return false;
+        }
+
+        Node<E> currentNode = head.root;
         int level = Node.bitPerNode * (depth - 1);
+
         while (level > 0) {
-            int index = ((newHead.size - 1) >> level) & mask;
+            int index = ((head.size - 1) >> level) & mask;
             Node<E> tmp, newNode;
+
             if (currentNode.child == null) {
                 currentNode.child = new LinkedList<>();
                 newNode = new Node<>();
@@ -157,21 +182,19 @@ public class PersistentArray<E> extends AbstractPersistentCollection<E> {
         return conj(newElement);
     }
 
-    private Node<E> getNode(Node<E> root, int index) {
+    private Node<E> getLeaf(Node<E> root, int index) {
         int level = bitPerLevel - Node.bitPerNode;
         Node<E> node = root;
-
         while (level > 0) {
             int tempIndex = (index >> level) & mask;
             node = node.child.get(tempIndex);
             level -= Node.bitPerNode;
         }
-
         return node;
     }
 
     private E get(Head<E> head, int index) {
-        return getNode(head.root, index).value.get(index & mask);
+        return getLeaf(head.root, index).value.get(index & mask);
     }
 
     @Override
